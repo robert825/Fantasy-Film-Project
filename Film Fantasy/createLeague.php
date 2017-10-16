43e2757600f7f9c48397369738930c58281b83b9
hed9pc <hed9pc@virginia.edu>
2017-10-16 11:16:45 -0400

    woeifjwo;

--- Film Fantasy/createLeague.php
+++ Film Fantasy/createLeague.php
@@ -5,49 +5,32 @@
 	$name = $season = $league = NULL;
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		if (empty($_POST['name'])) {
-			$key = $key . "Name of League, ";
-		}
-		else {
+		if (isSet($_POST['name'])) {
 			$name = trim($_POST['name']);
 			$_SESSION['name_global'] = trim($_POST['name']);
-			
 		}
-		if (empty($_POST['players'])) {
-			$key = $key . "Players, ";
-		}
-		else {
+		if (isSet($_POST['players'])) {
 			$players = trim($_POST['players']);
 			$_SESSION['players_global'] = trim($_POST['players']);
 		}
-		if (empty($_POST['movies'])) {
-			$key = $key . "Movies per player, ";
-		}
-		else {
+		if (isSet($_POST['movies'])) {
 			$movies = trim($_POST['movies']);
 			$_SESSION['movies_global'] = trim($_POST['movies']);
 		}
-		if (empty($_POST['season'])) {
-			$key = $key . "Length of season, ";
-		}
-		else {
+		if (isSet($_POST['season'])) {
 			$season = trim($_POST['season']);
 			$_SESSION['season_global'] = trim($_POST['season']);
 		}
-		if (empty($_POST['league'])) {
-			$key = $key . "Type of League, ";
-		}
-		else {
+		if (isSet($_POST['league'])) {
 			$league = trim($_POST['league']);
 			$_SESSION['league_global'] = trim($_POST['league']);
 		}
-
+		
 		if ($name != NULL && $players != NULL && $movies != NULL && $season != NULL && $league != NULL) {
 			header("Location: addPlayersPage.html");
 		}
 		else {
 			header("Location: createLeaguePage.html");
-			echo "<script type='text/javascript'>alert('Incomplete form fields! Please enter values for the following: \n' + $key);</script>";
 		}
 	}
 	?>
